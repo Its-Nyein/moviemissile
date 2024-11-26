f83356e102e4a9001479ca89a18a614e7a29caeb
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-11-26 13:53:58 +0630

    feat: complete functionality movies pagination

--- src/pages/movies/Movies.jsx
+++ src/pages/movies/Movies.jsx
@@ -1,5 +1,50 @@
+import { useEffect, useState } from "react";
+import { fetchMoviesDiscovers } from "../../services/fetcher";
+import MovieCard from "../../components/MovieCard";
+import Pagination from "../../UI/Pagination";
+import LoadingSpinner from "../../UI/LoadingSpinner";
+
 const Movies = () => {
-  return <div>Movies</div>;
+  const [movies, setMovies] = useState([]);
+  const [pages, setPages] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [sortBy, setSortBy] = useState("popularity.desc");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetchMoviesDiscovers(pages, sortBy)
+      .then((res) => setMovies(res?.results))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }, [pages, sortBy]);
+
+  if (loading) {
+    return <LoadingSpinner />;
+  }
+
+  return (
+    <div className="container mx-auto max-w-7xl px-4 my-5">
+      <h2 className="font-semibold uppercase text-[#353535] opacity-80">
+        Discover Movies
+      </h2>
+
+      {movies && (
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-5 gap-4 align-middle my-8">
+          {movies.map((movie) => (
+            <MovieCard key={movie.id} item={movie} type="movie" />
+          ))}
+        </div>
+      )}
+
+      <Pagination
+        onPageChange={setPages}
+        totalCount={totalPages}
+        currentPage={pages}
+        pageSize={20}
+      />
+    </div>
+  );
 };
 
 export default Movies;
